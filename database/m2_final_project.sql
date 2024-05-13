20bf8faf3f54baf626a1c2798ac1baf6d8339699
Annie Burnett <burnetao@gmail.com>
2024-05-13 12:47:52 -0600

    Making my project in GH

---
+++ database/m2_final_project.sql
@@ -0,0 +1,37 @@
+-- database m2_final_project
+BEGIN TRANSACTION;
+
+-- *************************************************************************************************
+-- Drop all db objects in the proper order
+-- *************************************************************************************************
+DROP TABLE IF EXISTS users;
+
+-- *************************************************************************************************
+-- Create the tables and constraints
+-- *************************************************************************************************
+
+--users (name is pluralized because 'user' is a SQL keyword)
+CREATE TABLE users (
+	user_id SERIAL,
+	username varchar(50) NOT NULL UNIQUE,
+	password_hash varchar(200) NOT NULL,
+	role varchar(50) NOT NULL,
+	name varchar(50) NOT NULL,
+	address varchar(100) NULL,
+	city varchar(50) NULL,
+	state_code char(2) NULL,
+	zip varchar(5) NULL,
+	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+-- *************************************************************************************************
+-- Insert some sample starting data
+-- *************************************************************************************************
+
+-- Users
+-- Password for all users is password
+INSERT INTO users (username, password_hash, role, name, address, city, state_code, zip) VALUES
+    ('user', '$2a$10$tmxuYYg1f5T0eXsTPlq/V.DJUKmRHyFbJ.o.liI1T35TFbjs2xiem','ROLE_USER',  'Jack O''Lantern', null, 'Cleveland', 'OH', '44123'),
+    ('admin','$2a$10$tmxuYYg1f5T0eXsTPlq/V.DJUKmRHyFbJ.o.liI1T35TFbjs2xiem','ROLE_ADMIN', 'Jill O''Lantern', null, 'Beverly Hills', 'CA', '90210');
+
+COMMIT TRANSACTION;
